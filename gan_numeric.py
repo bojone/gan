d4010d1be338f3c91f50e1639936c1a63152cfde
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-05-04 15:31:19 +0800

    Update gan_numeric.py

--- gan_numeric.py
+++ gan_numeric.py
@@ -406,3 +406,53 @@ anim = animation.FuncAnimation(fig=fig,
                                interval=100)
 
 anim.save('line.gif', writer='imagemagick', dpi=80)
+
+
+
+# GAN-QP带L2正则 动画
+
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+
+T = 70
+h = 0.1
+lamb = 1
+
+
+def F(t, x):
+    varphi1, varphi2, theta1, theta2 = x
+    norm = np.sqrt(varphi1**2 + varphi2**2)
+    dot = varphi1 * theta1 + varphi2 * theta2
+    return [- theta1 - theta1 * dot / norm / lamb - 0.1 * varphi1,
+            - theta2 - theta2 * dot / norm / lamb - 0.1 * varphi2,
+            varphi1,
+            varphi2
+            ]
+
+
+ts = np.arange(0, T, h)
+xs = solve_ivp(F, (0, T), [0.1, 0.2, 0.2, 0.1], method='RK45', t_eval=ts)
+varphi1, varphi2, theta1, theta2 = xs.y
+
+
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.scatter(0, 0, 30, color='red')
+ax.set_xlim(-0.3, 0.3)
+ax.set_ylim(-0.3, 0.3)
+ax.set_xlabel('$\\theta_1$')
+ax.set_ylabel('$\\theta_2$')
+line, = plt.plot(theta1[:2], theta2[:2], color='blue')
+
+def animate(i):
+    line.set_data(theta1[:i], theta2[:i])
+    return line,
+
+anim = animation.FuncAnimation(fig=fig,
+                               func=animate,
+                               frames=range(12, len(theta1), 9),
+                               interval=100)
+
+anim.save('line.gif', writer='imagemagick', dpi=80)
